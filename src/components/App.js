7d9adcc1992b1d122548d3863ac0d8897fdbb331
Lee <lee.gould@devgarden.se>
2020-05-18 17:16:19 +0200

    updates...

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../sass/main.scss";
+// import history from "./common/history";
+export default class componentName extends Component {
+  render() {
+    return (
+      <div>
+        <h1>welcome to react</h1>
+      </div>
+    );
+  }
+}
