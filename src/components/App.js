a31e0352a7d4ebc32e09fa291e56f9530d49390d
Lee <lee.gould@devgarden.se>
2020-05-18 17:34:53 +0200

    updates...

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+
 import "../sass/main.scss";
+
 // import history from "./common/history";
 export default class componentName extends Component {
   render() {
