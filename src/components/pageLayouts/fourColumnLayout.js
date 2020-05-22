6db72af7dbc573c6e63031e4aa01b655cd9f8700
Lee <lee.gould@devgarden.se>
2020-05-22 09:31:04 +0200

    updates...

--- src/components/pageLayouts/fourColumnLayout.js
+++ src/components/pageLayouts/fourColumnLayout.js
@@ -1,39 +1,13 @@
 import React, { Component } from "react";
 
-import AButton from "../elements/aButton";
-
 class fourColumnLayout extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      buttons: {
-        hasButtons: false,
-        classes: {
-          mainClass: "button",
-          orangeButton: "orange-btn",
-          greyButton: "grey-btn"
-        },
-        hrefs: {
-          link1: "#",
-          link2: "#",
-          link3: "#",
-          link4: "#"
-        },
-        title: {
-          title1: "read more",
-          title2: "visit site",
-          title3: "donate",
-          title4: "read more"
-        }
-      }
-    };
+    this.state = {};
   }
   render() {
-    let { params } = this.props;
-    let { buttons } = this.state;
-    console.log("buttons object : " + JSON.stringify(buttons));
     return (
-      <section className={params}>
+      <section>
         <div className="container">
           <div className="row">
             <div className="col-3">
@@ -43,13 +17,6 @@ class fourColumnLayout extends Component {
                 et pariatur. Adipisicing mollit culpa excepteur do labore esse
                 aliqua.
               </p>
-              <AButton
-                classes={
-                  buttons.classes.mainClass + " " + buttons.classes.orangeButton
-                }
-                hrefs={buttons.hrefs.link1}
-                title={buttons.title.title1}
-              />
             </div>
 
             <div className="col-3">
@@ -59,13 +26,6 @@ class fourColumnLayout extends Component {
                 et pariatur. Adipisicing mollit culpa excepteur do labore esse
                 aliqua.
               </p>
-              <AButton
-                classes={
-                  buttons.classes.mainClass + " " + buttons.classes.greyButton
-                }
-                hrefs={buttons.hrefs.link1}
-                title={buttons.title.title2}
-              />
             </div>
 
             <div className="col-3">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,31 +17,6 @@ class homePage extends Component {
       title: {
         value: "welcome to my portfolio website",
         color: "title-text"
-      },
-
-      twoColumnLayout: {
-        fontColors: {
-          lightOrange: "light-orange-color",
-          darkOrange: "dark-orange-color",
-          darkGrey: "dark-grey-color",
-          white: "white"
-        },
-        paddingTopBottom: "padding-top-bottom",
-        title: "about me",
-        title1: "my background",
-        bodyText:
-          "Use it to describe your credentials, expertise, and goals. What’s the best way to start? The following exercises can be helpful in figuring all of that out, and will help you determine what to include based on your target audience. Spend about five minutes on each question. You can use the ‘example’ answers to give you an idea of what that answer might sound like in its final form, but be sure to use your own words. ",
-        bodyText1:
-          "You can use the ‘example’ answers to give you an idea of what that answer might sound like in its final form, but be sure to use your own words. "
-      },
-      fourColumnLayout: {
-        background: {
-          lightOrange: "  light-orange",
-          darkOrange: " dark-orange",
-          darkGrey: " dark-grey",
-          lightGrey: " dark-grey"
-        },
-        paddingTopBottom: "padding-top-bottom"
       }
     };
   }
@@ -52,22 +27,10 @@ class homePage extends Component {
     return (
       <React.Fragment>
         <HeaderTag title={title} />
-        <TwoColumnsLayout params={twoColumnLayout} />
-        <FourColumnsLayout
-          params={
-            fourColumnLayout.background.lightOrange +
-            " " +
-            fourColumnLayout.paddingTopBottom
-          }
-        />
+        <TwoColumnsLayout />
+        <FourColumnsLayout />
         <ThreeColumnLayout />
-        <FourColumnsLayout
-          params={
-            fourColumnLayout.background.darkGrey +
-            " " +
-            fourColumnLayout.paddingTopBottom
-          }
-        />
+        <FourColumnsLayout />
         <ThreeColumnLayoutTwo />
       </React.Fragment>
     );
